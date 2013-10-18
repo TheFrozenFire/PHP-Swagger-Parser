cec9bcbeaf4c641e71579f614a12c6fffdac2e6d
Justin Martin <frozenfire@thefrozenfire.com>
2013-10-18 16:38:51 -0700

    Added ApiDeclaration suite with traversal test.

---
+++ tests/ApiDeclaration/TraveralTest.php
@@ -0,0 +1,67 @@
+<?php
+namespace Swagger;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\Stdlib\Hydrator;
+
+class TraversalTest extends TestCase {
+    /**
+     * Test a deep traversal of an Api Declaration
+     *
+     * @dataProvider provideApiDeclaration
+     * 
+     * @param Swagger\Document $document
+     */
+    public function testTraversal(Document $document) {
+        $hydrator = $this->getHydrator();
+        
+        $values = $hydrator->extract($document);
+        foreach($values as $value) {
+            if($value instanceof Swagger\Document) {
+                $this->testTraversal($value);
+            } elseif(is_array($value)) {
+                foreach($value as $subDocument) {
+                    if($value instanceof Swagger\Document) {
+                        $this->testTraversal($subDocument);
+                    }
+                }
+            }
+        }
+    }
+    
+    public function getHydrator() {
+        $hydrator = new Hydrator\ClassMethods;
+        $hydrator->addFilter('getDocumentProperty', new Hydrator\Filter\MethodMatchFilter('getDocumentProperty'), Hydrator\Filter\FilterComposite::CONDITION_AND);
+        $hydrator->addFilter('getSubDocument', new Hydrator\Filter\MethodMatchFilter('getSubDocument'), Hydrator\Filter\FilterComposite::CONDITION_AND);
+        $hydrator->addFilter('getSubDocuments', new Hydrator\Filter\MethodMatchFilter('getSubDocuments'), Hydrator\Filter\FilterComposite::CONDITION_AND);
+        $hydrator->addFilter('getDocument', new Hydrator\Filter\MethodMatchFilter('getDocument'), Hydrator\Filter\FilterComposite::CONDITION_AND);
+        
+        return $hydrator;
+    }
+    
+    public function provideApiDeclarationDocument() {
+        $data = array();
+        
+        foreach(glob(__DIR__.'/fixtures/apideclaration*.json') as $filename) {
+            $data[] = array(
+                file_get_contents($filename)
+            );
+        }
+        
+        return $data;
+    }
+    
+    public function provideApiDeclaration() {
+        $documents = $this->provideApiDeclarationDocument();
+        
+        $data = array();
+        
+        foreach($documents as $document) {
+            $data[] = array(
+                new ApiDeclaration($document[0])
+            );
+        }
+        
+        return $data;
+    }
+}
